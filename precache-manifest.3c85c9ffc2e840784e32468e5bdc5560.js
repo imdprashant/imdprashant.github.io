fafc157d97189f4d448ad87a9452ba905f31f5fc
imdprashant <dungeon_masters@outlook.com>
2020-11-02 20:53:08 +0530

    first commit

---
+++ precache-manifest.3c85c9ffc2e840784e32468e5bdc5560.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d44837c074fd1b625d45990168f9863",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01690e26a957787bd9a2",
+    "url": "/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "da6fd7a02cd995498cb5",
+    "url": "/static/js/2.8e73fc4f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.8e73fc4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01690e26a957787bd9a2",
+    "url": "/static/js/main.1e1e5430.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "a9a8a632ce52b4502b7fc86ba19d3b7a",
+    "url": "/static/media/prashantdubey.a9a8a632.jpg"
+  },
+  {
+    "revision": "9749d9c10a1c472826cfd9200fe0a962",
+    "url": "/static/media/resume.9749d9c1.pdf"
+  }
+]);
